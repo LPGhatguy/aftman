d94688a63aa22225ac2d987228a3d08c4cf2ac80
Lucien Greathouse <me@lpghatguy.com>
2021-08-27 18:10:57 -0400

    Config resolution, temporary aftman.toml in root

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,10 @@
-use std::path::PathBuf;
+use std::io::{self, BufWriter, Write};
+use std::path::{Path, PathBuf};
 
-use anyhow::format_err;
+use anyhow::{bail, format_err};
+use fs_err::OpenOptions;
+
+use crate::manifest::{DEFAULT_GLOBAL_MANIFEST, MANIFEST_FILE_NAME};
 
 pub fn config_dir() -> anyhow::Result<PathBuf> {
     let mut path =
@@ -8,4 +12,33 @@ pub fn config_dir() -> anyhow::Result<PathBuf> {
 
     path.push(".aftman");
     Ok(path)
+}
+
+/// Attempt to initialize Aftman's global configuration files so that users can
+/// edit them by hand.
+pub fn initialize_global_config() -> anyhow::Result<()> {
+    let base_dir = config_dir()?;
+    fs_err::create_dir_all(&base_dir)?;
+
+    let manifest_path = base_dir.join(MANIFEST_FILE_NAME);
+    write_if_not_exists(&manifest_path, DEFAULT_GLOBAL_MANIFEST.trim())?;
+
+    Ok(())
+}
+
+fn write_if_not_exists(path: &Path, contents: &str) -> anyhow::Result<()> {
+    let mut file = match OpenOptions::new().create_new(true).write(true).open(path) {
+        Ok(file) => BufWriter::new(file),
+        Err(err) => {
+            if err.kind() == io::ErrorKind::AlreadyExists {
+                return Ok(());
+            }
+
+            bail!(err);
+        }
+    };
+
+    file.write_all(contents.as_bytes())?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,39 @@ mod manifest;
 mod tool_name;
 mod tool_spec;
 
+use std::env;
+
+use anyhow::Context;
 use structopt::StructOpt;
 
 use crate::cli::Args;
+use crate::config::initialize_global_config;
+use crate::manifest::Manifest;
 
-fn main() {
+fn run() -> anyhow::Result<()> {
+    let exe_name =
+        env::current_exe().context("Failed to discover the name of the Aftman executable")?;
+
+    let current_dir = env::current_dir().context("Failed to find current working directory")?;
+
+    let manifests = Manifest::discover(&current_dir)?;
+    println!("Manifests: {:#?}", manifests);
+
+    // TODO: Resolve our current exe name against all manifests from our current
+    // directory.
+
+    initialize_global_config()?;
+
     let args = Args::from_args();
 
-    println!("{:#?}", args);
+    println!("Args: {:#?}", args);
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("{:?}", err);
+        std::process::exit(1);
+    }
 }
