f165e48d992e9c0c8d09bfafbac1ff1c5a44eeba
Lucien Greathouse <me@lpghatguy.com>
2022-07-01 22:57:57 -0400

    Clean up fallback code

--- src/system_path/mod.rs
+++ src/system_path/mod.rs
@@ -7,6 +7,7 @@ mod unix;
 #[cfg(windows)]
 mod windows;
 
+#[allow(unreachable_code)]
 pub fn init(home: &Home) -> anyhow::Result<()> {
     // Users can define this environment variable to force Aftman to interact
     // with the user's PATH like a Unix machine. This is helpful for running
@@ -20,12 +21,10 @@ pub fn init(home: &Home) -> anyhow::Result<()> {
         return windows::init(home);
     }
 
-    #[cfg(not(any(windows, unix)))]
-    {
-        return Ok(());
-    }
+    Ok(())
 }
 
+#[allow(unreachable_code)]
 pub fn add(home: &Home) -> anyhow::Result<bool> {
     if cfg!(unix) || env::var("AFTMAN_PATH_UNIX").is_ok() {
         return unix::add(home);
@@ -36,9 +35,6 @@ pub fn add(home: &Home) -> anyhow::Result<bool> {
         return windows::add(home);
     }
 
-    #[cfg(not(any(windows, unix)))]
-    {
-        log::debug!("Not adding value to path because this platform is not supported.");
-        return Ok(false);
-    }
+    log::debug!("Not adding value to path because this platform is not supported.");
+    Ok(false)
 }
